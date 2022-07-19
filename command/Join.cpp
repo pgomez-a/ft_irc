12f7bfbc1304738633ef9f379d6010f81903feee
pgomez-a <pablogomezalvarez02@gmail.com>
2022-07-19 19:25:28 +0200

    Update JOIN command - Add split commas

--- command/Join.cpp
+++ command/Join.cpp
@@ -1,4 +1,5 @@
 #include "Join.hpp"
+#include "word_composition.hpp"
 
 Join::Join(void) {
 	_command_name = "JOIN";
@@ -11,34 +12,33 @@ Channel	*Join::get_channel(void)
 	return &(_channel_iterator->second);
 }
 
-/*
- * If a JOIN is successful, the user receives a JOIN message as
-   confirmation and is then sent the channel's topic (using RPL_TOPIC) and
-   the list of users who are on the channel (using RPL_NAMREPLY), which
-   MUST include the user joining.
-  *
-*/
 int	Join::_effect(server_t &server, client_t &client)
 {
-	int		r = 0;
+	int			r = 0;
+	std::list<std::string>	token_list;
 
 	if (_argc)
 	{
-		_channel_iterator = server.find_channel(_argt[0]);
-		if (_channel_iterator == server.channel_map_end())
-			r = server.add_new_channel( _argt[0], "", "", _channel_iterator);
-		if (r == BAD_CHANNEL_NAME)
-			return ERR_NOSUCHCHANNEL;
-		r = _channel_iterator->second.add_member(&client);
-		if (r == BANNED)
-			return ERR_BANNEDFROMCHAN;
-		if (r == CHANNEL_IS_FULL)
-			return ERR_CHANNELISFULL;
-		_rest = _channel_iterator->second.get_topic();
-		send_to_client( ":" + client.get_originname() + " JOIN :" + _channel_iterator->second.get_name() + "\r\n", client);
-		reply_to_client( _rest.size() ? RPL_TOPIC: RPL_NOTOPIC, client, server, this);
-		reply_to_client( RPL_NAMREPLY, client, server, this);
-		return RPL_ENDOFNAMES;
+		token_list = split_token(',', _argt[0]);
+		for (std::list<std::string>::iterator channel = token_list.begin(); channel != token_list.end(); ++channel)
+		{
+			_channel_iterator = server.find_channel(*channel);
+			if (_channel_iterator == server.channel_map_end())
+				r = server.add_new_channel(*channel, "", "", _channel_iterator);
+			if (r == BAD_CHANNEL_NAME)
+				return ERR_NOSUCHCHANNEL;
+			r = _channel_iterator->second.add_member(&client);
+			if (r == BANNED)
+				return ERR_BANNEDFROMCHAN;
+			if (r == CHANNEL_IS_FULL)
+				return ERR_CHANNELISFULL;
+			_rest = _channel_iterator->second.get_topic();
+			send_to_client( ":" + client.get_originname() + " JOIN :" + _channel_iterator->second.get_name() + "\r\n", client);
+			reply_to_client( _rest.size() ? RPL_TOPIC: RPL_NOTOPIC, client, server, this);
+			reply_to_client( RPL_NAMREPLY, client, server, this);
+			reply_to_client( RPL_ENDOFNAMES, client, server, this);
+		}
+		return 0;
 	}
 	return ERR_NEEDMOREPARAMS;
 }
--- command/Privmsg.cpp
+++ command/Privmsg.cpp
@@ -8,17 +8,15 @@ Privmsg::Privmsg(void) {
 
 int	Privmsg::_effect(server_t &server, client_t &client)
 {
-	client_t*							receiver;
+	client_t*					receiver;
 	std::list<std::string>				token_list;
-	std::list<std::string>::iterator	nick;
 
 	if (!_argc)
 		return ERR_NORECIPIENT;
 	if (_rest.size() == 0)
 		return ERR_NOTEXTTOSEND;
 	token_list = split_token(',', _argt[0]);
-	nick = token_list.begin();
-	while (nick != token_list.end())
+	for (std::list<std::string>::iterator nick = token_list.begin(); nick != token_list.end(); ++nick)
 	{
 		if ((*nick)[0] == '#')
 		{
@@ -34,7 +32,6 @@ int	Privmsg::_effect(server_t &server, client_t &client)
 				return ERR_NOSUCHNICK;
 			send_to_client( ":" + client.get_originname() + " PRIVMSG " + *nick + " :" + _rest + "\r\n", *receiver);
 		}
-		++nick;
 	}
 	return 0;
 }
