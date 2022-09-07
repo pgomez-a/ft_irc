34f57b6aaacaf204188a8884a69c1a7a3cce7b3f
Pablo Gomez alvarez <pgomez-a@c2r5s4.42madrid.com>
2022-09-07 19:55:26 +0200

    Modify client script

--- test/clients/create_client.sh
+++ test/clients/create_client.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-for i in {1..1023}
+for i in {1..100}
 do
 	echo 'echo "PASS pass"' > "client$i.sh"
 	echo 'sleep 1' >> "client$i.sh"
 	echo "echo \"NICK c$i\"" >> "client$i.sh"
 	echo 'sleep 1' >> "client$i.sh"
 	echo 'echo "USER u 0 0 0 0"' >> "client$i.sh"
+	echo 'sleep 100' >> "client$i.sh"
 done
--- test/clients/execute_client.sh
+++ test/clients/execute_client.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-for i in {1..1023}
+for i in {1..100}
 do
 	bash "client$i.sh" | nc -c localhost 6667&
-	sleep 0.1
+	sleep 0.01
 done
